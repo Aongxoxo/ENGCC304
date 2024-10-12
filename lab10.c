8ddde74fe15713292555eb79ebea014447437096
ไก่ทอดตัวใหญ่ <jiramate_ta67@live.rmutl.ac.th>
2024-10-12 22:33:30 +0700

    Update lab10.c

--- lab10.c
+++ lab10.c
@@ -1,18 +1,24 @@
 #include <stdio.h>   
-#include <string.h> 
+#include <string.h>  
+#include <ctype.h> 
 
 int main() {
-    char input[100];   
+    char input[100];    
     int text = 1;       
 
     printf("Enter word: ");
-    scanf("%s", input); 
-    int len = strlen(input); 
+    scanf("%s", input);  
+
+    int len = strlen(input);  
 
    
-    for (int i = 0; i < len / 2; i++) { 
+    for (int i = 0; i < len; i++) {
+        input[i] = tolower(input[i]);
+    }
+
+    for (int i = 0; i < len / 2; i++) {  
         if (input[i] != input[len - 1 - i]) {  
-            text = 0;   
+            text = 0;  
             break;      
         }
     }
