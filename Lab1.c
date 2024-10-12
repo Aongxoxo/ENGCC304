2ddb84f949c2ffe81c4de4f9d12470bdb4c962a9
ไก่ทอดตัวใหญ่ <jiramate_ta67@live.rmutl.ac.th>
2024-10-12 21:36:39 +0700

    Update Lab1.c

--- Lab1.c
+++ Lab1.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+
 int main() {
-    printf ("hellow world");
-    printf ("\nMy name Jame");
+    char Name[50] ;
+    int  Age = 0 ;
+    printf( "Enter your name: " ) ;
+    scanf( "%s", Name ) ;
+    printf( "Enter your age: " ) ;
+    scanf( "%d", &Age ) ;
+    printf( "- - - - - -\n" ) ;
+    printf( "Hello %s \n", Name ) ; //TODO: #14 Say hello to user.
+    printf( "Age = %d\n", Name ) ; //TODO: #15 display user their age number plus 1.
+
     return 0 ;
-}
+    
+}//end main function
