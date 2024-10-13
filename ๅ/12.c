8426ab0a68e798f9d82d0fcf790d19cc0ef53496
ไก่ทอดตัวใหญ่ <jiramate_ta67@live.rmutl.ac.th>
2024-10-13 23:18:51 +0700

    Update 12.c

--- ๅ/12.c
+++ ๅ/12.c
@@ -7,11 +7,11 @@ struct Student {
 } typedef S;
 char getGrade(float score) {
     if (score >= 80) return 'A';
-    else if (score >= 75) return 'B';
+    else if (score >= 75) return 'B+';
     else if (score >= 70) return 'B';
-    else if (score >= 65) return 'C';
+    else if (score >= 65) return 'C+';
     else if (score >= 60) return 'C';
-    else if (score >= 55) return 'D';
+    else if (score >= 55) return 'D+';
     else if (score >= 50) return 'D';
     else return 'F';
 }
