1ed86f4c594315600322fbf98b38cd75566d3e31
ไก่ทอดตัวใหญ่ <jiramate_ta67@live.rmutl.ac.th>
2024-10-14 16:34:54 +0700

    Update Lab5.c

--- Lab5.c
+++ Lab5.c
@@ -9,21 +9,21 @@ int main() {
         printf("Please enter a number only.\n");
     } else {
         if (score >= 80) {
-            printf("A!\n");
+            printf("A !\n");
         } else if (score >= 75 && score < 80) {
-            printf("B+!\n");
+            printf("B+ !\n");
         } else if (score >= 70 && score < 75) {
-            printf("B!\n");
+            printf("B !\n");
         } else if (score >= 65 && score < 70) {
-            printf("C+!\n");
+            printf("C+ !\n");
         } else if (score >= 60 && score < 65) {
-            printf("C!\n");
+            printf("C !\n");
         } else if (score >= 55 && score < 60) {
-            printf("D+!\n");
+            printf("D+ !\n");
         } else if (score >= 50 && score < 55) {
-            printf("D!\n");
+            printf("D !\n");
         } else {
-            printf("F!\n");
+            printf("F !\n");
         }
     }
 
