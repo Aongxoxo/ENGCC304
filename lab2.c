412f482c7b27eed20147cd01d1b6f7d1c8ccfbc0
ไก่ทอดตัวใหญ่ <jiramate_ta67@live.rmutl.ac.th>
2024-10-15 13:44:13 +0700

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -7,19 +7,19 @@ int main() {
     float number;
     char string[100];
     
-    printf("Enter a character:");
+    printf("Enter a character: ");
     scanf(" %c", &inputChar);
 
-    printf("Enter two integers:");
+    printf("\nEnter two integers: ");
     scanf("%d %d", &num1, &num2);
     
-    printf("Enter a floating-point number:");
+    printf("Enter a floating-point number: ");
     scanf("%f", &number);
 
-    printf("Enter a string:");
+    printf("Enter a string: ");
     scanf("%s", string);
 
-    printf("You entered character: %c\n", inputChar);
+    printf("\nYou entered character: %c\n", inputChar);
     printf("You entered integers: %d and %d\n", num1, num2);
     printf("You entered floating-point number: %.2f\n", number);
     printf("You entered string: %s\n", string);
